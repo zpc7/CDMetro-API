40fff87609dea0716d65ee0c795394e8f50f6f3c
zpc7 <zpc.excel@foxmail.com>
2020-03-07 10:56:24 +0800

    修复:404 response设置body会导致status变为200

--- app/middleware/notfound_handler.js
+++ app/middleware/notfound_handler.js
@@ -2,12 +2,14 @@
 module.exports = () => {
   return async function notFoundHandler(ctx, next) {
     await next();
-    if (ctx.status === 404 && !ctx.body) {
-      if (ctx.acceptJSON) {
-        ctx.body = { error: '404 Not Found, PC' };
-      } else {
-        ctx.body = '<h1>404 Page Not Found, PC</h1>';
-      }
-    }
+    // if (ctx.status === 404 && !ctx.body) {
+    //   if (ctx.acceptJSON) {
+    //     ctx.body = { error: '404 Not Found, PC' };
+    //   } else {
+    //     ctx.body = '<h1>404 Page Not Found, PC</h1>';
+    //   }
+    // }
+    // 设置了 ctx.response.body 会导致status 404 --> 200
+    // ctx.body = 'set body';
   };
 };
