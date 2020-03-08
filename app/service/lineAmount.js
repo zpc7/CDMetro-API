c5d500dd55e114d9418b98eb7050b61455743df4
zpc7 <zpc.excel@foxmail.com>
2020-03-08 21:30:05 +0800

    添加不带查询参数的数据处理

--- app/service/lineAmount.js
+++ app/service/lineAmount.js
@@ -1,5 +1,6 @@
 'use strict';
 const Service = require('egg').Service;
+const _ = require('lodash');
 
 // 日期类型简写-全称对照表
 const enumCompareBoard = {
@@ -10,9 +11,37 @@ const enumCompareBoard = {
 
 class LineAmountService extends Service {
   async findAll(query) {
-    const total = await this.ctx.model.LineAmount.count();
-    const list = await this.ctx.model.LineAmount.findAll(query);
-    return { total, list };
+    const ctx = this.ctx;
+    const response = {
+      total: 0,
+      list: [],
+    };
+    // TODO: 处理查询参数
+    console.log('------------');
+    console.log('query', query);
+    response.total = await ctx.model.DayAmount.count();
+    const list = await this.ctx.model.LineAmount.findAll();
+
+    list.forEach(item => {
+      const key = _.findIndex(response.list, ['date', item.date]);
+      if (key === -1) {
+        response.list.push({
+          id: item.id,
+          date: item.date,
+          dateType: item.dateType,
+          lineData: [{
+            lineId: item.lineId,
+            lineAmount: item.amount,
+          }],
+        });
+      } else {
+        response.list[key].lineData.push({
+          lineId: item.lineId,
+          lineAmount: item.amount,
+        });
+      }
+    });
+    return response;
   }
   async findById(id) {
     return await this.ctx.model.LineAmount.findByPk(id);
